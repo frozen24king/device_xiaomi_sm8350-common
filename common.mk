888a18927ea653000e117429b0340c4825b5beba
Chenyang Zhong <zhongcy95@gmail.com>
2023-03-01 09:04:23 -0500

    sm8350-common: build 64-bit audio HIDL as well
    libnotifyaudiohal.so from the elliptic ultrasonic proximity sensor
    needs the 64-bit version of android.hardware.audio@7.1.so.
    
    Change-Id: bf155aeefd52fa00586b318caf7522c98f84aed5
    Signed-off-by: frozenking <frozenking1240196@gmail.com>

--- common.mk
+++ common.mk
@@ -42,8 +42,8 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl:32 \
-    android.hardware.audio.effect@7.0-impl:32 \
+    android.hardware.audio@7.1-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl
 
